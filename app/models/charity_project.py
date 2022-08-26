2b3148b3866a052231d07c746b5a5a630293c400
Arseniy Gaintsev <arseniy.gaintsev@gmail.com>
2022-08-27 00:03:16 +0300

    tests ok

--- app/models/charity_project.py
+++ app/models/charity_project.py
@@ -6,8 +6,9 @@ from sqlalchemy.orm import relationship
 
 # Импортируем базовый класс для моделей.
 from app.core.db import Base, DonationMixin
+from app.models.basemodel import StartModel
 
 
-class CharityProject(Base, DonationMixin):
+class CharityProject(Base, StartModel):
     name = Column(String(100), unique=True, nullable=False)
     description = Column(Text, nullable=False)
